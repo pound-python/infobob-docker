bd03aae81361d82c8e0cfb1e802f9860868c19d2
Aaron Gallagher <_@habnab.it>
2019-01-06 19:06:39 -0800

    Read the znc password out of the environment.
    This means I can commit the config file without leaking the secret.

--- infobob/launch.sh
+++ infobob/launch.sh
@@ -1,4 +1,8 @@
-#!/bin/sh -eux
+#!/bin/sh -eu
+jq --arg znc_password "${ZNC_PASSWORD}" \
+    '.irc.password = $znc_password' \
+    config-template.json >config.json
+set -x
 sqlite3 <db.schema /app/infobob.sqlite
 chown infobob: /app/infobob.sqlite
-exec chpst -u infobob twistd --pidfile= -n infobat infobob.cfg
+exec chpst -u infobob twistd --pidfile= -n infobat config.json
