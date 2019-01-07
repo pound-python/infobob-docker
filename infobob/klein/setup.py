c39264df91f2dc9f6631cf7a2c8ebd2c426c0c1a
Aaron Gallagher <_@habnab.it>
2019-01-07 00:24:55 -0800

    Reposition the infobob source.
    It's going to go in infobob/src

--- infobob/klein/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="klein",
-    version="0.0.1",
-    description="werkzeug + twisted.web",
-    packages=["klein"],
-    install_requires=["Twisted", "werkzeug", "mock"]
-)
